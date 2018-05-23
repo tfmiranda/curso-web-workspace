ae6a4870c7a211addc0fb18d67d540ea7d13a6a6
tfmiranda <miranda.tiagof@gmail.com>
2018-05-22 21:53:32 -0300

    gitignore fix, clean folders, eval calc project

--- aula-4/project/app.js
+++ aula-4/project/app.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -19,5 +24,61 @@ app.get('/login', (req, res) => {
 app.get('/select', (req, res) => {
 	res.render('layout');	
 });  
+
+app.get('/api', (req, res) => {
+	//TODO Return a HTML guide page of API
+	res.send('GET /api response');
+});
+
+app.get('/api/somar', (req, res) => {
+	let a = parseInt(req.query.a);
+	let b = parseInt(req.query.b);
+	res.json(a+b);
+});
+
+app.post('/api/somar', (req, res) => {
+	let values = req.body.values;
+	values = JSON.parse(values);
+	let total = 0;
+	values.forEach(element => {
+		total = total + element;
+	});
+	res.json(total);
+});
+
+app.get('/api/subtrair', (req, res) => {
+	let a = parseInt(req.query.a);
+	let b = parseInt(req.query.b);
+	res.json(a-b);
+});
+
+app.post('/api/subtrair', (req, res) => {
+	let values = req.body.values;
+	values = JSON.parse(values);
+	let total = 0;
+	values.forEach(element => {
+		total = total - element;
+	});
+	res.json(total);
+});
+
+app.get('/api/multiplicar', (req, res) => {
+	let a = parseInt(req.query.a);
+	let b = parseInt(req.query.b);
+	res.json(a*b);
+});
+
+app.get('/api/dividir', (req, res) => {
+	let a = parseInt(req.query.a);
+	let b = parseInt(req.query.b);
+	if(b==0) {
+		res.json({resultado: null, erro: `Divisão por zero`});
+	}else{
+		res.json(a/b);
+	}	
+});
+
+//TODO Move calc functions to aula-3 project
+//TODO api/trig, api/random, api/latex
 
 app.listen(3000, () => console.log('Servidor escutando na porta 3000!'));
